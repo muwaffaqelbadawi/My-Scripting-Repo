231b8ae9154c0eba5e7262fa23e9424f5f08fc27
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2025-03-21 20:35:28 +0200

    add definition to git_Command_Reference + add My_workspace_Server

--- Command Reference/git_Command_Reference.ps1
+++ Command Reference/git_Command_Reference.ps1
@@ -1,18 +1,47 @@
 # Define username
 git.exe config --global user.name "Your Name"
+
 # Define user Email
 git.exe config --global user.email "you@example.com"
-# Define a proxy
+
+# show status of local repo
+git.exe status
+
+# set git bash proxy
 git.exe config --global https.proxy "https://IP:port"
+
+# fetch remote repo
 git.exe fetch origin
+
+# Merge regardless of unrelated history
 git.exe merge origin/main --allow-unrelated-histories
-git.exe branch --set-upstream-to=origin/main main
+
+# sync loca repo branch with remote repo branch
+git.exe branch --set-upstream-to=origin/"<remote_branch>" "local_branch"
+
+# add local repo to remote repo
 git.exe remote add origin "path/to/remote/repository"
+
+# delete local branch
 git.exe branch -d master
-git.exe push origin --delete master
+
+# delete remore branch
+git.exe push origin --delete "<branch>"
+
+# show all local commits
 git.exe log --oneline
+
+# hard reset local repo commit
 git.exe reset --hard "<commit-hash>"
-git.exe pull "<remote>" "<branch>"
-git.exe rm "path/to/file"
+
+# pull changed from remore repo branch
+git.exe pull "<remote_repo>" "<branch>"
+
+# delete a file from local repo (You need to commit changes)
+git.exe rm -rf --chached "path/to/file"
+
+# Configure local repo branch type globally (git config settins)
 git.exe config --global init.defaultBranch main
-git.exe rm -rf --chached "file"
+
+# track remote repo branch
+git.exe branch --track origin/main main
